c95f5253f16f2ac0b47d21004599adb2eb85b4cd
daferarte <daferarte@gmail.com>
2024-06-15 15:53:00 -0500

    Creacion de repositorio

---
+++ primera_sesion/ciclowhile.py
@@ -0,0 +1,10 @@
+numero=0
+mul=6
+print (f"Tabla del {mul}")
+while numero <= 10:
+    # print("7 X ", numero,"=", (numero*7))
+    print(f"{mul}X{numero}={mul*numero}")
+    # numero = numero+1
+    numero+=2
+    
+print("fin")
---
+++ primera_sesion/sentencias.py
@@ -0,0 +1,43 @@
+# Comentario simple
+"""
+    Ejemplos de sentencias:
+    +, -, *, /, %  # operadores
+    = # igual a
+    if
+    else
+    while
+    for
+""" 
+
+# sentencia de un tipo
+a=2+3+500
+
+# sentencia de mas de un tipo
+b=2+4*5/6+7
+
+# vector
+vectors=[1,2,3,4,5,6,7,8,9]
+
+# condicionales 
+
+if a==b:
+    print("Son iguales")
+elif a<=b:
+    print("B es mayor")
+elif a>=b:
+    print("A es mayor")
+else: 
+    print("Error")
+    
+
+
+#----------------------------------------------------------------
+d=1
+e="1"
+
+if d==e:
+    print("son iguales")
+    
+
+#----------------------------------------------------------------
+print("termine")
