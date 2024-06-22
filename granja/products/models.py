ab7fbbea428fc781877902a581911be1137e1ff5
daferarte <daferarte@gmail.com>
2024-06-22 15:57:40 -0500

    Agregando django y template

---
+++ granja/products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class categories(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre')
+    description = models.TextField(verbose_name='Descripción')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Creado')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Editado')
+    
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        
+    def __str__(self):
+        return str(self.name)
