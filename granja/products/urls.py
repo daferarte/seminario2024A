8f08fb0531b4531338b628dc7d9d9dabf7373c28
daferarte <daferarte@gmail.com>
2024-07-06 15:23:24 -0500

    agregando mas api CRUD

--- granja/products/urls.py
+++ granja/products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import listProducts, productDetail, Product_APIView_List
+from .views import listProducts, productDetail, Product_APIView_List, Product_APIView, Product_APIView_Detail
 
 urlpatterns = [
     path('productos/', listProducts, name="productos"),
     path('productos/<int:product_id>', productDetail, name="producto"),
-    path('v1/prod', Product_APIView_List.as_view())
+    path('v1/prod', Product_APIView_List.as_view()),
+    path('v1/prod/add', Product_APIView.as_view()),
+    path('v1/prod/<int:pk>', Product_APIView_Detail.as_view()),
 ]
--- granja/products/views.py
+++ granja/products/views.py
@@ -35,3 +35,37 @@ class Product_APIView_List(APIView):
         product = products.objects.all()
         serializer = ProductsSerializers(product, many=True)
         return Response(serializer.data)
+    
+class Product_APIView(APIView):
+    def post (self, request, format=None):
+        serializer = ProductsSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class Product_APIView_Detail(APIView):
+    
+    def get_object(self, pk):
+        try:
+            return products.objects.get(pk=pk)
+        except products.DoesNotExist:
+            raise Http404
+    
+    def get (self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductsSerializers(product)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductsSerializers(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        product = self.get_object(pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
