e1f00a70e28994c836303c6f857bbb374543c75a
daferarte <daferarte@gmail.com>
2024-07-06 17:03:51 -0500

    agregando docker

--- granja/granja/settings.py
+++ granja/granja/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'granja.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "granja",
+        "USER": "postgres",
+        "PASSWORD": "12345",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
